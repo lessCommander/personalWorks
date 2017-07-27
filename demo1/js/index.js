5eefcd3a7b2e0e8f5479c47e9bd2820cc562d38f
LuckyGitName <2310975964@qq.com>
2017-07-27 21:49:54 +0800

    modify normal

--- demo1/js/index.js
+++ demo1/js/index.js
@@ -1,7 +1,29 @@
 window.onload = function(){
-	var aLi1 = document.querySelectorAll('.item');
+	var aTitle = document.querySelectorAll('.pro_title li'),
+		aLi1 = document.querySelectorAll('.item'),
+		i,
+		iCurrent = 0;
 
-	for(var i = 0; i < aLi1.length; i++){
+	for(i = 0; i < aTitle.length; i++){
+		aTitle[i].index = i;
+	}
+
+	for(i = 0; i < aTitle.length; i++){
+		aTitle[i].onmouseenter = function(){
+			this.className = 'pro_title_active';
+		};
+		aTitle[i].onmouseleave = function(){
+			this.className = '';
+			aTitle[iCurrent].className = 'pro_title_active';
+		};
+		aTitle[i].onclick = function(){
+			aTitle[iCurrent].className = '';
+			this.className = 'pro_title_active';
+			iCurrent = this.index;
+		};
+	}
+
+	for(i = 0; i < aLi1.length; i++){
 		aLi1[i].onmouseenter = function(){
 			this.className = this.className + ' ' + 'active';
 		};
