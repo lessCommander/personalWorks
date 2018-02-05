e167d8a4c84e696bfa120f32b13fba72e6ef6b62
LuckyGitName <2310975964@qq.com>
2018-02-05 16:52:34 +0800

    create lrts project

---
+++ lrts/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="app">
+        <div class="content">
+            <div>
+                <img class="banner" src="./assets/img/d_banner.jpg" alt="">
+            </div>
+            <div class="home-icons">
+                <homeicons></homeicons>
+            </div>
+            <div class="home-category">
+                
+            </div>
+        </div>
+        <footer>
+            
+        </footer>
+    </div>
+</template>
+
+<script>
+    import homeicons from './components/homeicons'
+
+    export default {
+        components: {
+            homeicons
+        }
+    }
+</script>
+
+<style>
+    body, div, p, ul, li{
+        margin: 0;
+        padding: 0;
+    }
+    .banner{
+        width: 100%;
+    }
+</style>
---
+++ lrts/src/components/homeiconChild.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="homeicon-wrap">
+		<div>
+			<img :src="homeiconimg" alt="">
+		</div>
+		<div class="homeicon-wrap-name">书城</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props:[
+			'homeiconimg'
+		]
+	}
+</script>
+
+<style scoped>
+	.homeicon-wrap-name{
+		width: 39px;
+		text-align: center;
+	}
+</style>
