a074d2e00bc7c4bfeb6f2d7ce2737319eb552ebd
LuckyGitName <2310975964@qq.com>
2018-02-22 17:23:09 +0800

    add broadcaster finished

---
+++ lrts/src/components/home/broadcaster/broadcaster-list-item.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="broadcaster-list-item">
+		<div class="media">
+			<div class="media-left media-middle">
+				<a href="javascript:">
+					<slot name="broadcaster-list-item-img-slot"></slot>
+				</a>
+			</div>
+			<div class="media-body">
+				<h4 class="media-heading">{{brname}}</h4>
+				<div>最近更新：{{brauthor}}</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		props:[
+			'brname',
+			'brauthor'
+		]
+	}
+
+</script>
+
+<style scoped>
+	.broadcaster-list-item{
+		padding: 10px 5px;
+		color: #666;
+		border-bottom: 1px solid #eee;
+	}
+	.media-body h4{
+		color: #333;
+	}
+	.media-body div{
+		font-size: 12px;
+		color: #999;
+	}
+</style>
---
+++ lrts/src/components/home/publicRecommend3.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="recommend3-inner">
+		<ul>
+			<li v-for="item in recommend3arr">
+				<a href="#">
+					<div class="recommend3-img">
+						<img :src="item.img" alt="">
+					</div>
+					<div class="recommend3-title">
+						{{item.txt}}
+					</div>
+				</a>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default{
+		props:[
+			'recommend3arr'
+		]
+	}
+</script>
+
+<style scoped>
+	.recommend3-inner > ul{
+		list-style: none;
+	}
+	.recommend3-inner > ul > li{
+		float: left;
+		margin: 0 15px;
+		overflow: hidden;
+	}
+	.recommend3-title{
+		width: 70px;
+		height: 30px;
+		margin-top: 5px;
+		text-align: center;
+		overflow: hidden;
+	}
+</style>
